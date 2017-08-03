8bbefa6233bd8bc1ef3c12e362e882d418364eff
Kushal K S V S <kkushal32@gmail.com>
2017-08-03 10:36:08 -0700

    script to compile and run

--- include/freetype/config/ftoption.h
+++ include/freetype/config/ftoption.h
@@ -119,7 +119,7 @@ FT_BEGIN_HEADER
   /* rendering technology that produces excellent output without LCD       */
   /* filtering.                                                            */
   /*                                                                       */
-/* #define FT_CONFIG_OPTION_SUBPIXEL_RENDERING */
+#define FT_CONFIG_OPTION_SUBPIXEL_RENDERING
 
 
   /*************************************************************************/
